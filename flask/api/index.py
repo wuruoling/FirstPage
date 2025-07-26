53489a44fb3e7323210bd5fc84325b8ea9c1d226
wuruoling <wuruoling746@gmail.com>
2025-07-26 15:54:46 +0800

    Update index.py

--- flask/api/index.py
+++ flask/api/index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return render_template('welcome.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
